1686b3963f201b7efa1b7ee7f015b6e6d5719c78
Kirill Kulakov <kulakov@cs.karelia.ru>
2013-10-21 18:18:41 +0400

    implement script 'add user'

--- scripts/geo2tag.php
+++ scripts/geo2tag.php
@@ -2,8 +2,26 @@
 
 include "add-user.inc";
 
+function return_xmlrpc_error($errno,$errstr,$errfile=NULL,$errline=NULL
+       ,$errcontext=NULL){
+    global $xmlrpc_server;
+    if(!$xmlrpc_server)die("Error: $errstr in '$errfile', line '$errline'");
+
+    header("Content-type: text/xml; charset=UTF-8");
+    print(xmlrpc_encode(array(
+        'faultCode'=>$errno
+        ,'faultString'=>"Remote XMLRPC Error from
+          ".$_SERVER['HTTP_HOST'].": $errstr in at $errfile:$errline"
+    )));
+    die();
+} 
+set_error_handler('return_xmlrpc_error');
+
 /* create server */
 $xmlrpc_server = xmlrpc_server_create();
+
+global $dbconn;
+$dbconn = pg_connect("dbname=geo2tag user=geo2tag password=geo2tag");
 
 /* register methods */
 xmlrpc_server_register_method($xmlrpc_server, "addUser", "adduser_func");
@@ -13,7 +31,8 @@ $request_xml = $HTTP_RAW_POST_DATA;
 
 $response = xmlrpc_server_call_method($xmlrpc_server, $request_xml, '');
 
-print $response;
+header('Content-Type: text/xml');
+echo $response;
 
 xmlrpc_server_destroy($xmlrpc_server);
 ?>

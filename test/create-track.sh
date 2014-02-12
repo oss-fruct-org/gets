0c11f0e9c7ccfb70b11d91b4ba5da8a4909b8d8e
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-02-12 14:01:54 +0400

    Create track method

---
+++ test/create-track.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#./reset-database.sh > /dev/null
+source ./config.sh
+
+echo "Executing createTrack method"
+curl -d@- "${GETS_SERVER}/createTrack.php" <<-EOF
+<request><params>
+
+<auth_token>$(cat token.txt)</auth_token>
+<name>newtrack</name>
+<description>Desc channel</description>
+<url>http://example.com</url>
+
+</params></request>
+EOF

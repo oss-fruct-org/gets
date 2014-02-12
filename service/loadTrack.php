f5584cf2220d033958824492f75a3dc8cc3697bc
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-02-12 12:36:02 +0400

    loadTrack method

---
+++ service/loadTrack.php
@@ -0,0 +1,81 @@
+<?php
+include_once('include/methods_url.inc');
+include_once('include/utils.inc');
+include_once('include/public_token.inc');
+
+header ('Content-Type:text/xml');
+
+$xml_post = file_get_contents('php://input');
+if (!$xml_post) {
+    send_error(1, 'Error: no input file');
+    die();
+}
+
+libxml_use_internal_errors(true);
+$dom = new DOMDocument();
+$dom->loadXML($xml_post);
+
+if (!$dom) {
+    send_error(1, 'Error: resource isn\'t XML document.');
+    die();
+}
+
+if (!$dom->schemaValidate('schemes/loadTrack.xsd')) {
+    send_error(1, 'Error: not valid input XML document.');
+    die();
+}
+
+$auth_token_element = $dom->getElementsByTagName('auth_token');
+$is_auth_token_defined = $auth_token_element->length > 0;
+
+$name_element = $dom->getElementsByTagName('name');
+$is_name_defined = $name_element->length > 0;
+
+$old_token = true;
+if ($is_auth_token_defined) {
+    $data_array['auth_token'] = $auth_token_element->item(0)->nodeValue;
+    $old_token = false;
+} else {
+    $token = read_public_token();
+
+    // No token available, trying to receive it from geo2tag server
+    if (!$token) {
+        $token = receive_public_token();
+        $old_token = false;
+
+        if (!$token) {
+            send_error(1, 'Error: can\'t receive new token');
+        }
+    }
+
+    $data_array['auth_token'] = $token;
+}
+
+$data_array['channel'] = 'tr_' . $name_element->item(0)->nodeValue;
+$data_array['amount'] = 100;
+
+$data_json = json_encode($data_array);
+$response_json = process_request(FILTER_CHANNEL_METHOD_URL, $data_json, 'Content-Type:application/json');
+
+if (!$response_json) {
+    send_error(1, 'Error: problem with request to geo2tag.');
+    die();
+}
+
+$response_array = json_decode($response_json, true);
+if (!$response_array) {
+    send_error(1, 'Error: can\'t decode data from geo2tag.');
+    die();
+}
+
+$response_code = check_errors($response_array['errno']);
+if ($response_code !== "Ok") {
+    send_error(1, $response_code);
+    die();
+}
+
+
+var_dump($response_json);
+
+?>
+
--- test/reset-database.sh
+++ test/reset-database.sh
@@ -31,10 +31,10 @@ DROP TABLE users;
 DROP SEQUENCE users_seq;
 EOF
 
-psql geo2tag geo2tag -c "\i ${GEO2TAG_HOME}/scripts/base.sql"
-psql geo2tag geo2tag -c "\i ${GETS_HOME}/db/category.pg"
-psql geo2tag geo2tag -c "\i ${GETS_HOME}/db/track.pg"
-psql geo2tag geo2tag -c "delete from users;"
+#psql geo2tag geo2tag -c "\i ${GEO2TAG_HOME}/scripts/base.sql"
+#psql geo2tag geo2tag -c "\i ${GETS_HOME}/db/category.pg"
+#psql geo2tag geo2tag -c "delete from users;"
 
-psql geo2tag geo2tag -c "insert into users (email, login, password) values ('gets_user@example.com', 'gets_user', 'gets_user_password');"
-psql geo2tag geo2tag -c "insert into users (email, login, password) values ('test_user@example.com', 'test_user', 'test_user_password');"
+#psql geo2tag geo2tag -c "insert into users (email, login, password) values ('gets_user@example.com', 'gets_user', 'gets_user_password');"
+#psql geo2tag geo2tag -c "insert into users (email, login, password) values ('test_user@example.com', 'test_user', 'test_user_password');"
+psql geo2tag geo2tag < testdb1.sql

79f147cf2c852eb5da524fcb1cc6c4bac2b45f86
davydovsky <davydovs@cs.karelia.ru>
2014-02-26 15:49:34 +0400

    add subscribe method

--- service/subscribe.php
+++ service/subscribe.php
@@ -2,10 +2,58 @@
 include_once('include/methods_url.inc');
 include_once('include/utils.inc');
 
-if (!empty($_POST)) {
-	$data = json_encode($_POST);
-	echo process_request(SUBSCRIBE_METHOD_URL, $data);
-} else {
-	echo "Nothing";
+header('Content-Type:text/xml');
+
+$xml_post = file_get_contents('php://input');
+if (!$xml_post) {
+    send_error(1, 'Error: no input file');
+    die();
 }
+
+libxml_use_internal_errors(true);	
+$dom = new DOMDocument();
+$dom->loadXML($xml_post);
+			
+if (!$dom) {
+    send_error(1, 'Error: resource isn\'t XML document.');
+    die();
+}
+
+if (!$dom->schemaValidate('schemes/subscribe.xsd')) {
+    send_error(1, 'Error: not valid input XML document.');
+    die();
+}
+
+$auth_token_element = $dom->getElementsByTagName('auth_token');
+$channel_name_element = $dom->getElementsByTagName('channel');
+
+$data_array = array();
+$data_array['auth_token'] = $auth_token_element->item(0)->nodeValue;
+$data_array['channel'] = $channel_name_element->item(0)->nodeValue;
+
+$data_json = json_encode($data_array);
+if (!$data_json) {
+    send_error(1, 'Error: can\'t convert data to json.');
+    die();
+}
+
+$response_json =  process_request(SUBSCRIBE_METHOD_URL, $data_json, 'Content-Type:application/json');
+if (!$response_json) {
+    send_error(1, 'Error: problem with request to geo2tag.');
+    die();
+}
+
+$response_array = json_decode($response_json, true);
+if (!$response_array) {
+    send_error(1, 'Error: can\'t decode data from geo2tag.');
+    die();
+}
+
+$response_code = check_errors($response_array['errno']);
+if ($response_code != 'Ok') {
+    send_error(1, $response_code);
+    die();
+}
+
+send_result(0, 'success', '');
 ?>

34697c2ac95f314075f3b30f96285f1e011ca4b2
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-05-14 15:20:41 +0400

    Update tag extension method

--- scripts/geo2tag.php
+++ scripts/geo2tag.php
@@ -38,6 +38,7 @@ xmlrpc_server_register_method($xmlrpc_server, "deleteDupTags", "deleteDupTags_fu
 xmlrpc_server_register_method($xmlrpc_server, "deleteTag", "deleteTag_func");
 xmlrpc_server_register_method($xmlrpc_server, "deleteChannel", "deleteChannel_func");
 xmlrpc_server_register_method($xmlrpc_server, "deleteTag2", "deleteTag2_func");
+xmlrpc_server_register_method($xmlrpc_server, "updateTag", "updateTag_func");
 
 /* process request */
 $request_xml = $HTTP_RAW_POST_DATA;

c6406ad56af2f582ee7831c755071e7d6a78791c
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-05-19 15:33:12 +0400

    Delete tag using google token

--- service/deletePoint.php
+++ service/deletePoint.php
@@ -32,57 +32,7 @@ $channel_name = get_request_argument($dom, 'channel');
 $point_name = get_request_argument($dom, 'name');
 $point_category = get_request_argument($dom, 'category_id');
 
-try {
-    auth_set_token($auth_token);
-    $auth_token = auth_get_geo2tag_token();
-} catch (GetsAuthException $e) {
-    send_error(1, $e->getMessage());
-    die();
-}
-
-// First, receive list of channels, subscribed by user 
-$data_array = array();
-$data_array['auth_token'] = $auth_token;
-
-$data_json = json_encode($data_array);
-$response_json = process_request(SUBSCRIBED_CHANNELS_METHOD_URL, $data_json, 'Content-Type:application/json');
-
-if (!$response_json) {
-    send_error(1, 'Error: problem with request to geo2tag.');
-    die();
-}
-
-$response_array = json_decode($response_json, true);
-if (!$response_array) {
-    send_error(1, 'Error: can\'t decode data from geo2tag.');
-    die();
-}
-
-if ($response_array['errno'] !== 0) {
-    send_error(1, 'Error: can\'t receive channel list from geo2tag server');
-    die();
-}
-
-
-$channel_name_found = null;
-if (array_key_exists('channels', $response_array)) {
-    foreach ($response_array['channels'] as $channel) {
-        $subscribed_channel_name = $channel['name'];
-
-        if ($subscribed_channel_name === $channel_name) {
-            $channel_name_found = $channel_name;
-            break;
-        }
-    }
-}
-
-// Don't allow to delete tags from channel not subscribed
-if (!$channel_name_found) {
-    send_error(1, 'Error: channel not subscribed');
-    die();
-}
-
-$xmlrpc_array = array('channel' => $channel_name);
+$xmlrpc_array = array('channel' => $channel_name, 'gets_token' => $auth_token);
 if ($point_name)
     $xmlrpc_array['name'] = $point_name;
 if ($point_category)
@@ -92,9 +42,10 @@ $xmlrpc_request = xmlrpc_encode_request('deleteTag2', $xmlrpc_array);
 
 $xmlrpc_response =  process_request(ADDITIONAL_FUNCTIONS_METHOD_URL, $xmlrpc_request, 'Content-Type: text/xml');
 $xmlrpc = xmlrpc_decode($xmlrpc_response);
+dump_error_log($xmlrpc_response);
 
 if (is_array($xmlrpc) && xmlrpc_is_fault($xmlrpc)) {
-    send_error(1, 'Error: internal error: can\'t delete channeld');
+    send_error(1, 'Error: internal error: can\'t delete tag');
     die();
 }
 
--- service/deleteTrack.php
+++ service/deleteTrack.php
@@ -30,61 +30,9 @@ if (!$dom->schemaValidate('schemes/deleteTrack.xsd')) {
 $auth_token = get_request_argument($dom, 'auth_token');
 $track_name = get_request_argument($dom, 'name');
 
-try {
-    auth_set_token($auth_token);
-    $auth_token = auth_get_geo2tag_token();
-} catch (GetsAuthException $e) {
-    send_error(1, $e->getMessage());
-    die();
-}
-
-// First, receive list of channels, subscribed by user 
-$data_array = array();
-$data_array['auth_token'] = $auth_token;
-
-$data_json = json_encode($data_array);
-$response_json = process_request(SUBSCRIBED_CHANNELS_METHOD_URL, $data_json, 'Content-Type:application/json');
-
-if (!$response_json) {
-    send_error(1, 'Error: problem with request to geo2tag.');
-    die();
-}
-
-$response_array = json_decode($response_json, true);
-if (!$response_array) {
-    send_error(1, 'Error: can\'t decode data from geo2tag.');
-    die();
-}
-
-if ($response_array['errno'] !== 0) {
-    send_error(1, 'Error: can\'t receive channel list from geo2tag server');
-    die();
-}
-
-
-$channel_name_found = null;
-
-if (array_key_exists('channels', $response_array)) {
-    foreach ($response_array['channels'] as $channel) {
-        $channel_name = $channel['name'];
-
-        if ($track_name === $channel_name) {
-            $channel_name_found = $channel_name;
-            break;
-        }
-    }
-}
-
-if (!$channel_name_found) {
-    send_error(1, 'Error: channel not subscribed');
-    die();
-}
-
 $xmlrpc_request = xmlrpc_encode_request('deleteChannel', 
-    array('login' => GEO2TAG_USER, 
-          'password' => GEO2TAG_PASSWORD,
-#          'user' => 'unknown', 
-          'channel' => $channel_name_found));
+    array('gets_token' => $auth_token,
+          'channel' => $channel_name_found);
 
 $xmlrpc_response =  process_request(ADDITIONAL_FUNCTIONS_METHOD_URL, $xmlrpc_request, 'Content-Type: text/xml');
 $xmlrpc = xmlrpc_decode($xmlrpc_response);

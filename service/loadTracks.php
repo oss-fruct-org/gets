d4df4b8604370e5219ca2deb1a4ed0fd71499e79
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-06-03 18:06:43 +0400

    Radius filtering tracks

--- service/loadTracks.php
+++ service/loadTracks.php
@@ -37,6 +37,12 @@ $auth_token = get_request_argument($dom, 'auth_token');
 $category_name = get_request_argument($dom, 'category_name');
 $space_arg = get_request_argument($dom, 'space');
 
+$radius = get_request_argument($dom, 'radius', 0);
+$longitude = get_request_argument($dom, 'longitude', 0);
+$latitude = get_request_argument($dom, 'latitude', 0);
+
+$is_radius_filter = ($radius !== 0);
+
 $space = SPACE_ALL;
 if ($space_arg) {
     if ($space_arg === 'public') {
@@ -72,7 +78,7 @@ if ($space === SPACE_ALL || $space === SPACE_PUBLIC) {
 }
 
 // Find id of requested category
-$category_id = null;
+$requested_category_id = null;
 if ($category_name) {
     try {
         $categories = get_categories();
@@ -80,12 +86,12 @@ if ($category_name) {
         foreach ($categories as $category) {
             // Category name equals requested name
             if ($category['name'] === $category_name) {
-                $category_id = $category['id'];
+                $requested_category_id = $category['id'];
                 break;
             }
         }
 
-        if (!$category_id) {
+        if (!$requested_category_id) {
             send_error(1, "Wrong category name");
             die();
         }
@@ -96,32 +102,37 @@ if ($category_name) {
 }
 
 function process_subscribed_channels($response_array, $access, &$resp) {
-    global $category_id;
+    global $requested_category_id;
 
     foreach ($response_array['channels'] as $channel) {
+        if (isset($channel['channel'])) {
+            $channel = $channel['channel'];
+        }
+
         $channel_name = $channel['name'];
-        $channel_desc = $channel['description'];
-        $channel_url = $channel['url'];
+        $channel_desc = get_array_element($channel, 'description', '');;
+        $channel_url = get_array_element($channel, 'url', '');;
 
         $channel_description = null;
-        $channel_id = null;
+        $channel_category_id = null;
         $channel_lang = null;
         $channel_hname = null;
 
         $desc_arr = json_decode($channel_desc, true);
         if ($desc_arr) {
             $channel_description = get_array_element($desc_arr, 'description');
-            $channel_id = get_array_element($desc_arr, 'category_id');
+            $channel_category_id = get_array_element($desc_arr, 'category_id');
             $channel_lang = get_array_element($desc_arr, 'lang');
             $channel_hname = get_array_element($desc_arr, 'hname');
         }
 
-        if ($channel_id && stripos($channel_name, "tr_") === 0 && ($category_id == null || $category_id == $channel_id)) {
+        if (stripos($channel_name, "tr_") === 0 
+                && ($requested_category_id === null || $requested_category_id === $channel_category_id)) {
             $resp .= '<track>';
             $resp .= '<name>' . $channel_name . '</name>';
 
             $resp .= '<description>' . $channel_description . '</description>';
-            $resp .= '<category_id>' . $channel_id . '</category_id>';
+            $resp .= '<category_id>' . $channel_category_id . '</category_id>';
 
             if ($channel_lang)
                 $resp .= '<lang>' . $channel_lang . '</lang>';
@@ -138,17 +149,39 @@ function process_subscribed_channels($response_array, $access, &$resp) {
 
 $resp = '<tracks>';
 
-
 try {
+    $method = SUBSCRIBED_CHANNELS_METHOD_URL;
+    $request_array = Array();
+
+    if ($is_radius_filter) {
+        $method = FILTER_CIRCLE_METHOD_URL;
+        $request_array = Array('radius' => $radius, 'latitude' => $latitude, 'longitude' => $longitude, 'time_from' => '01 01 1999 00:00:00.000',
+                'time_to' => '01 01 2199 00:00:00.000');
+    }
+
     if ($space === SPACE_PUBLIC || $space === SPACE_ALL) {
-        $response_array = process_json_request(SUBSCRIBED_CHANNELS_METHOD_URL, Array(), $public_token);
+        $response_array = process_json_request($method, $request_array, $public_token);
         process_subscribed_channels($response_array, 'r', $resp);
     }
 
     if ($space === SPACE_PRIVATE || $space === SPACE_ALL) {
-        $response_array = process_json_request(SUBSCRIBED_CHANNELS_METHOD_URL, Array(), $private_token);
+        $response_array = process_json_request($method, $request_array, $private_token);
         process_subscribed_channels($response_array, 'rw', $resp);
     }
+
+    /* else {
+        if ($space === SPACE_PUBLIC || $space === SPACE_ALL) {
+            $response_array = process_json_request(FILTER_CIRCLE_METHOD_URL, 
+                    Array('radius' => $radius, 'latitude' => $latitude, 'longitude' => $longitude), $public_token);
+            process_channels_in_radius($response_array, 'r', $resp);
+        }
+
+        if ($space === SPACE_PUBLIC || $space === SPACE_ALL) {
+            $response_array = process_json_request(FILTER_CIRCLE_METHOD_URL, 
+                    Array('radius' => $radius, 'latitude' => $latitude, 'longitude' => $longitude), $private_token);
+            process_channels_in_radius($response_array, 'r', $resp);
+        }
+    }*/
 } catch (Exception $e) {
     send_error(1, $e->getMessage());
     invalidate_public_token();

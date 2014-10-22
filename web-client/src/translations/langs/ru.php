c8b21e02c8afd9999d0f915d5dc88e9132f12d02
davydovsky <davydovs59@gmail.com>
2014-10-22 19:27:13 +0400

    add dynamic approach for the translations

---
+++ web-client/src/translations/langs/ru.php
@@ -0,0 +1,88 @@
+<?php
+$translations = array(
+    
+    // Common strings
+    'points' => 'Точки',//'' => '',
+    'tracks' => 'Маршруты', 
+    'polygons' => 'Полигоны',  
+    'sign-in' => 'Войти', 
+    'sign-out' => 'Выйти', 
+    
+    // Common label strings
+    'name' => 'Имя', 
+    'description' => 'Описание', 
+    'url' => 'Ссылка', 
+    'required-fields' => 'Обязательные поля', 
+    
+    // Main tracks widget strings
+    'add-track' => 'Добавить маршрут', 
+    'update' => 'Обновить', 
+    'filter-tracks' => 'Фильтр маршрутов', 
+    'filter-by-name' => 'Фильтр по имени: ', 
+    'filter-by-category' => 'Фильтр по категории: ', 
+    'filter-by-space' => 'Фильтр по пространству: ', 
+    'filter-all' => 'Все', 
+    'filter-only-my' => 'Только мои', 
+    'filter-only-public' => 'Только публичные', 
+    'track-name' => 'Имя маршрута', 
+    'filter' => 'Фильтр',
+    
+    // Add Track widget strings
+    'add-track-title' => 'Добавить маршрут',
+    'category' => 'Категория', 
+    'language' => 'Язык', 
+    'track-add-success' => 'Маршрут был успешно добавлен', 
+    
+    // Edit track widget strings 
+    'edit-track-title' => 'Редактировать маршрут', 
+    'track-edit-success' => 'Маршрут был успешно отредактирован',
+      
+    // Track Info widget strings
+    'track-info-title' => 'Информация о маршруте', 
+    'add-point' => 'Добавить точку', 
+    'edit-track' => 'Редактировать маршрут', 
+    'remove-track' => 'Удалить маршрут', 
+    'show-on-map' => 'Показать на карте', 
+    'point-list' => 'Список точек', 
+    'track-remove-text' => 'Вы уверены что хотите удалить этот маршрут? (Действие не может быть отменено)', 
+    'track-remove-success' => 'Маршрут был успешно удален', 
+    
+    // Point Info widget strings
+    'point-info-title' => 'Информация о точке', 
+    'edit-point' => 'Редактировать точку', 
+    'remove-point' => 'Удалить точку', 
+    'no-image' => 'Нет изображения', 
+    'point-remove-text' => 'Вы уверены что хотите удалить эту точку? (Действие не может быть отменено)', 
+    'point-remove-success' => 'Точка была успешно удалена', 
+    
+    // Add Point widget strings
+    'add-point-title' => 'Добавить точку', 
+    'coordinates' => 'Координаты', 
+    'latitude' => 'Широта', 
+    'longitude' => 'Долгота', 
+    'active-radius' => 'Активный радиус (метры)', 
+    'picture' => 'Изображение', 
+    'audio-track' => 'Аудио трек', 
+    'paste-url-or' => 'Вставить ссылку (URL) или', 
+    'upload-file' => 'загрузить файл',
+    'upload' => 'Загрузить', 
+    'clear' => 'Очистить', 
+    'close' => 'Закрыть', 
+    'point-add-success' => 'Точка была успешно добавлена', 
+    
+    // Edit Point widget strings
+    'edit-point-title' => 'Редактировать точку', 
+    'coordinates' => 'Координаты', 
+    'point-edit-success' => 'Точка была успешно отредактирована', 
+    
+    // Points Main widget 
+    'search-points' => 'Поиск точек', 
+    'show-search-area' => 'Показать область поиска', 
+    'space' => 'Пространство', 
+    'all' => 'Все', 
+    'public' => 'Публичные',
+    'private' => 'Приватные', 
+    'search' => 'Поиск'
+);
+?>
+

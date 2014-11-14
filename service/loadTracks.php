4b11058f871ee2a84b54e16cc67ba942d477558e
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-11-14 17:41:37 +0300

    Rewrite loadTracks.php using direct database access

--- service/loadTracks.php
+++ service/loadTracks.php
@@ -61,136 +61,92 @@ if ($space === SPACE_ALL && !$auth_token) {
     $space = SPACE_PUBLIC;
 }
 
-$private_token = $auth_token;
-$public_token = null;
+$dbconn = pg_connect('host=localhost dbname=geo2tag user=geo2tag');
+
+// email where query
+$where_arr = array();
+$email_where_arr = array();
+if ($space === SPACE_ALL || $space === SPACE_PRIVATE) {
+    auth_set_token($auth_token);
+    $email = auth_get_google_email();
+    $email_escaped = pg_escape_string($dbconn, $email);
+    session_commit();
+
+    $email_where_arr[] = "users.email='${email_escaped}'";
+}
 
 if ($space === SPACE_ALL || $space === SPACE_PUBLIC) {
-    $public_token = get_public_token();
-
-    if (!$public_token) {
-        send_error(1, 'Error: can\'t receive public token');
-        die();
-    }
+    $email_escaped = pg_escape_string($dbconn, GEO2TAG_EMAIL);
+    $email_where_arr[] = "users.email='${email_escaped}'";
 }
 
-// Find id of requested category
-$requested_category_id = null;
+$query =  'SELECT DISTINCT channel.name, channel.description, channel.url FROM channel ';
+$query .= 'INNER JOIN tag ON tag.channel_id = channel.id ';
+$query .= 'INNER JOIN subscribe ON channel.id = subscribe.channel_id ';
+$query .= 'INNER JOIN users ON subscribe.user_id=users.id ';
+
 if ($category_name) {
-    try {
-        $categories = get_categories();
-
-        foreach ($categories as $category) {
-            // Category name equals requested name
-            if ($category['name'] === $category_name) {
-                $requested_category_id = $category['id'];
-                break;
-            }
-        }
-
-        if (!$requested_category_id) {
-            send_error(1, "Wrong category name");
-            die();
-        }
-    } catch (Exception $e) {
-        send_error(1, $e->getMessage());
-        die();
-    }
+    $query .= 'INNER JOIN category ON safe_cast_to_json(channel.description)->>\'category_id\'=category.id::text ';
 }
 
-function process_subscribed_channels($response_array, $access, &$resp, $is_incomplete_channel, $token, &$processed_channels) {
-    global $requested_category_id;
+$email_where = '(' . implode(' OR ', $email_where_arr) . ')';
 
-
-    if (!isset($response_array['channels'])) {
-        return;
-    }
-
-    foreach ($response_array['channels'] as $channel) {
-        if (isset($channel['channel'])) {
-            $channel = $channel['channel'];
-        }
-
-        $channel_name = $channel['name'];
-
-        if (!$channel_name || array_key_exists($channel_name, $processed_channels))
-            continue;
-
-        $processed_channels[$channel_name] = null;
-
-        if ($is_incomplete_channel) {
-            $channel_info = get_channel_info($channel_name, $token);
-            $channel_desc = $channel_info['description'];
-            $channel_url = $channel_info['url'];
-        } else {
-            $channel_desc = get_array_element($channel, 'description', '');;
-            $channel_url = get_array_element($channel, 'url', '');;
-        }
-
-        $channel_description = null;
-        $channel_category_id = null;
-        $channel_lang = null;
-        $channel_hname = null;
-
-        $desc_arr = json_decode($channel_desc, true);
-        if ($desc_arr) {
-            $channel_description = get_array_element($desc_arr, 'description');
-            $channel_category_id = get_array_element($desc_arr, 'category_id');
-            $channel_lang = get_array_element($desc_arr, 'lang');
-            $channel_hname = get_array_element($desc_arr, 'hname');
-        }
-
-        if ((stripos($channel_name, "tr_") === 0 || stripos($channel_name, "tr+") === 0) && ($requested_category_id === null || $requested_category_id == $channel_category_id)) {
-            $resp .= '<track>';
-            $resp .= '<name>' . htmlspecialchars($channel_name) . '</name>';
-
-            $resp .= '<description>' . htmlspecialchars($channel_description) . '</description>';
-            $resp .= '<category_id>' . $channel_category_id . '</category_id>';
-
-            if ($channel_lang)
-                $resp .= '<lang>' . htmlspecialchars($channel_lang) . '</lang>';
-
-            if ($channel_hname)
-                $resp .= '<hname>' . htmlspecialchars($channel_hname) . '</hname>';
-
-            $resp .= '<access>' . $access . '</access>';
-
-            $resp .= '</track>';
-        }
-    }
+$where_arr[] = $email_where;
+if ($category_name) {
+    $category_name_escaped = pg_escape_string($dbconn, $category_name);
+    $where_arr[] = "category.name='${category_name_escaped}'";
 }
+
+$where_arr[] = "(substr(channel.name, 0, 4)='tr+' OR substr(channel.name, 0, 4)='tr_')";
+
+# Distance where
+if ($is_radius_filter) {
+    $where_arr[] = "gets_geo_distance(tag.latitude, tag.longitude, ${latitude}, ${longitude}) < ${radius}";
+}
+
+$query .= 'WHERE ' . implode(' AND ', $where_arr) . ';';
+
+$result = pg_query($dbconn, $query);
 
 $resp = '<tracks>';
+while ($row = pg_fetch_row($result)) {
+    $channel_name = $row[0];
+    $channel_desc = $row[1];
+    $channel_url = $row[2];
 
-try {
-    $processed_channels = array();
+    $channel_description = null;
+    $channel_category_id = null;
+    $channel_lang = null;
+    $channel_hname = null;
 
-    $method = SUBSCRIBED_CHANNELS_METHOD_URL;
-    $request_array = Array();
-    $is_incomplete = false;
-
-    if ($is_radius_filter) {
-        $is_incomplete = true;
-        $method = FILTER_CIRCLE_METHOD_URL;
-        $request_array = Array('radius' => $radius, 'latitude' => $latitude, 'longitude' => $longitude, 'time_from' => '01 01 0001 00:00:00.000',
-                'time_to' => '01 01 2999 00:00:00.000');
+    $desc_arr = json_decode($channel_desc, true);
+    if ($desc_arr) {
+        $channel_description = get_array_element($desc_arr, 'description');
+        $channel_category_id = get_array_element($desc_arr, 'category_id');
+        $channel_lang = get_array_element($desc_arr, 'lang');
+        $channel_hname = get_array_element($desc_arr, 'hname');
     }
 
-    if ($space === SPACE_PRIVATE || $space === SPACE_ALL) {
-        $response_array = process_json_request($method, $request_array, $private_token);
-        process_subscribed_channels($response_array, 'rw', $resp, $is_incomplete, $private_token, $processed_channels);
-    }
+    $resp .= '<track>';
+    $resp .= '<name>' . htmlspecialchars($channel_name) . '</name>';
+    $resp .= '<description>' . htmlspecialchars($channel_description) . '</description>';
+    $resp .= '<category_id>' . $channel_category_id . '</category_id>';
 
-    if ($space === SPACE_PUBLIC || $space === SPACE_ALL) {
-        $response_array = process_json_request($method, $request_array, $public_token);
-        process_subscribed_channels($response_array, 'r', $resp, $is_incomplete, $public_token, $processed_channels);
-    }
-} catch (Exception $e) {
-    send_error(1, $e->getMessage());
-    invalidate_public_token();
-    die();
+    if ($channel_lang)
+        $resp .= '<lang>' . htmlspecialchars($channel_lang) . '</lang>';
+
+    if ($channel_hname)
+        $resp .= '<hname>' . htmlspecialchars($channel_hname) . '</hname>';
+
+    $resp .= '<access>' . $access . '</access>';
+    $resp .= '</track>';
 }
 
+
 $resp .= '</tracks>';
+
+pg_close($db_conn);
+
 send_result(0, 'success', $resp);
 
 ?>

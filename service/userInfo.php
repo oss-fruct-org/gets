4d0c3c0a4bb0b05d39e2095536151e1d5269bace
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-03-19 13:28:24 +0300

    Add name and image to userInfo output

--- service/userInfo.php
+++ service/userInfo.php
@@ -30,7 +30,7 @@ $auth_token = get_request_argument($dom, 'auth_token');
 try {
     auth_set_token($auth_token);
     $email = auth_get_google_email();
-    $escaped_email = htmlspecialchars($email);
+    $user_info = auth_get_google_user_info();
 } catch (GetsAuthException $e) {
     send_error(1, $e->getMessage());
     die();
@@ -41,8 +41,21 @@ $dbconn = pg_connect(GEO2TAG_DB_STRING);
 $user_is_admin = (is_user_admin($dbconn) > 0 ? true : false);
 $user_is_trusted = (is_user_trusted($dbconn) > 0 ? true : false);
 
+$escaped_email = htmlspecialchars($email);
+$escaped_image = htmlspecialchars($user_info["image"]);
+
+$escaped_first_name = htmlspecialchars($user_info["first_name"]);
+$escaped_middle_name = htmlspecialchars($user_info["middle_name"]);
+$escaped_last_name = htmlspecialchars($user_info["last_name"]);
+
 $response = '<userInfo>';
 $response .= "<email>{$escaped_email}</email>";
+$response .= "<first_name>{$escaped_first_name}</first_name>";
+$response .= "<middle_name>{$escaped_middle_name}</middle_name>";
+$response .= "<last_name>{$escaped_last_name}</last_name>";
+
+$response .= "<image>{$escaped_image}</image>";
+
 if (strcmp($email,GEO2TAG_EMAIL) == 0) {
     $response .= "<isCoreUser>true</isCoreUser>";
 }
@@ -52,8 +65,7 @@ if (strcmp($email,GEO2TAG_EMAIL) == 0 || $user_is_admin) {
 if ($user_is_trusted || strcmp($email,GEO2TAG_EMAIL) == 0 || $user_is_admin) {
     $response .= "<isTrustedUser>true</isTrustedUser>";
 }
+
 $response .= '</userInfo>';
 
 send_result(0, 'success', $response);
-
-?>

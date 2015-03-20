2db23d3a6a138b60f9acbb8cf21084071d24db71
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-03-20 13:35:30 +0300

    Move common code to header

--- service/auth/revokeToken.php
+++ service/auth/revokeToken.php
@@ -1,42 +1,20 @@
 <?php
 
-ini_set('session.use_cookies', 0);
-ini_set('session.use_trans_sid', 1);
-
 include_once('../include/methods_url.inc');
 include_once('../include/utils.inc');
 include_once('../include/auth.inc');
 include_once('../include/config.inc');
-include_once('../include/access_control.inc');
-
-header('Content-Type:text/xml');
-
-$xml_post = file_get_contents('php://input');
-if (!$xml_post) {
-    send_error(1, 'Error: no input file');
-    die();
-}
-
-libxml_use_internal_errors(true);
-$dom = new DOMDocument();
-$dom->loadXML($xml_post);
-
-if (!$dom) {
-    send_error(1, 'Error: resource isn\'t XML document.');
-    die();
-}
-
-if (!$dom->schemaValidate('../schemes/authToken.xsd')) {
-    send_error(1, 'Error: not valid input XML document.');
-    die();
-}
-
-$gets_token = get_request_argument($dom, "auth_token", null);
+include_once('../include/header.inc');
 
 try {
+    $dom = get_input_dom('../schemes/authToken.xsd');
+    $gets_token = get_request_argument($dom, "auth_token", null);
+
     auth_set_token($gets_token);
     auth_revoke();
-    send_result(0, 'success', $content);
-} catch (Exception $e) {
+    send_result(0, 'success', "success");
+} catch (GetsAuthException $e) {
     send_error(1, "Can't revoke token");
+} catch (Exception $e) {
+    send_error($e->getCode(), $e->getMessage());
 }

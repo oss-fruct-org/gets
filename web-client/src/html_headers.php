aa4d76e5a8b3c1d2b63acef1dfe76e5e9c03fc4e
Kirill Kulakov <kulakov@cs.karelia.ru>
2016-09-14 14:51:17 +0300

    configurable top menu; remove unused code

--- web-client/src/html_headers.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="styles/gets/style.css"/>';
-?>
--- web-client/src/images/generateImagesPaths.php
+++
@@ -1,9 +0,0 @@
-<?php
-$base_url = 'http://' . $_SERVER['SERVER_NAME'] . substr($_SERVER['REQUEST_URI'], 0, -strlen(basename(__FILE__)));
-
-$js_file = 'var MARKER_HOLE_IMAGE = "' . $base_url . 'icons/marker_hole.png";';
-$js_file .= 'var LOCATION_IMAGE = "' . $base_url . 'icons/location.png";';
-$js_file .= 'var TEMP_MARKER_IMAGE = "' . $base_url . 'icons/marker_hole_red.png";';
-
-echo $js_file;
-?>
